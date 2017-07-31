a690b7aa9cb76e11e3eb425cc55d6d6f61003762
ALippincott <alex.lippincott19@gmail.com>
2017-07-31 09:34:44 -0400

    More feedback changes

--- views/js/main.js
+++ views/js/main.js
@@ -273,7 +273,7 @@ function updatePositions() {
 	var items = document.querySelectorAll('.mover');
 	var documentMath = document.body.scrollTop / 1250;
 	for (var i = 0; i < items.length; i++) {
-		var phase = Math.sin(documentMath) + (i % 5);
+		var phase = Math.sin(documentMath + i % 5);
 		items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
 	}
 	// User Timing API to the rescue again. Seriously, it's worth learning.
